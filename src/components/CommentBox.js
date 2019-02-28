428a2ffbe6feb169b5e52ec004871fc21bdc2232
Marc Servat <marc.servat@smartcommerce.co>
2019-02-28 14:23:27 +0100

    WIP

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 
 class CommentBox extends Component{
+    state = {comment: ''};
+
+    handleChange = (e) => {
+        this.setState({ comment: e.target.value });s
+    };
+
+    handleSubmit = (e) => {
+      e.preventDefault();
+
+    };
+
     render() {
         return (
-            <div>Comment Box</div>
+            <form>
+                <h4>Comment Box</h4>
+                <textarea onChange={this.handleChange} value={this.state.comment}/>
+                <div>
+                    <button>Submit</button>
+                </div>
+            </form>
         )
     }
 }
--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,9 +1,20 @@
-import React, { Component } from 'react';
-import ReacDOM from 'react-dom';
-import App from '../../App';
+import React from 'react';
+import {shallow} from 'enzyme';
+import App from 'App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+let wrapped;
+
+beforeEach(() => {
+ wrapped = shallow(<App/>);
+});
 
 it('shows a comment box', () => {
- const div = document.createElement('div');
- ReacDOM.render(<App/>, div);
- ReacDOM.unmountComponentAtNode(div);
+ expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+
+it('shows a comment list', () => {
+ expect(wrapped.find(CommentList).length).toEqual(1);
 });
